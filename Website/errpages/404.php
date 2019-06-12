0c8f3fd3f4aa673f0091a9f93df88b7bd22b5865
Yannik Böltes @SEMPERCODING <admin@reol.dev>
2019-06-12 12:58:50 +0200

    Add files via upload

---
+++ Website/errpages/404.php
@@ -0,0 +1,12 @@
+
+    <div class="pos-a t-0 l-0 bgc-white w-100 h-100 d-f fxd-r fxw-w ai-c jc-c pos-r p-30">
+      <div class="mR-60"><img alt="#" src="<?php echo $config["site_url"]; ?>/assets/static/images/404.png"></div>
+      <div class="d-f jc-c fxd-c">
+        <h1 class="mB-30 fw-900 lh-1 c-red-500" style="font-size:60px">404</h1>
+        <h3 class="mB-10 fsz-lg c-grey-900 tt-c">Oops, Seite nicht gefunden</h3>
+        <p class="mB-30 fsz-def c-grey-700">Die von Ihnen gesuchte Seite existiert nicht oder wurde verschoben.</p>
+        <div><a href="dashboard" type="primary" class="btn btn-primary">Zurück zum Dashboard</a></div>
+      </div>
+    </div>
+
+
